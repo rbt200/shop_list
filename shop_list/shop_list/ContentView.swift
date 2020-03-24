90046258b49f4a6435e4b52e74209ea203cdd3a6
rbt200 <myesoap@mail.ru>
2020-03-24 06:26:31 -0700

    Initial commit

---
+++ shop_list/shop_list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shop_list
+//
+//  Created by Ivan Ivanov on 24.03.2020.
+//  Copyright © 2020 Ivan Ivanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
